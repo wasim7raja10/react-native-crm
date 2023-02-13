ccdaf038d95144c02fd999caf9337fadedbdd9aa
Wasim Raja <wasim7raja10@gmail.com>
2023-02-13 22:44:59 +0530

    added all screens in Navigation

--- src/Navigation.js
+++ src/Navigation.js
@@ -2,6 +2,10 @@ import { NavigationContainer } from "@react-navigation/native";
 import Welcome from "./screens/Welcome";
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import ListofRegions from "./screens/region/ListRegions";
+import ListbyRegion from "./screens/region/ListbyRegion";
+import NewCustomer from "./screens/customer/NewCustomer";
+import EditCustomer from "./screens/customer/EditCustomer";
 
 const Navigation = () => {
   const Stack = createNativeStackNavigator();
@@ -9,6 +13,10 @@ const Navigation = () => {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Welcome">
         <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="List of Regions" component={ListofRegions} />
+        <Stack.Screen name="List by Region" component={ListbyRegion} />
+        <Stack.Screen name="New Customer" component={NewCustomer} />
+        <Stack.Screen name="Edit Customer" component={EditCustomer} />
       </Stack.Navigator>
     </NavigationContainer>
   );
