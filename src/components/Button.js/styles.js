1e7c1426c3ea28a9ee1f3eb85d88b080c02f75b4
Wasim Raja <wasim7raja10@gmail.com>
2023-02-13 23:02:50 +0530

    button compoent

---
+++ src/components/Button.js/styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    button: {
+      marginTop: "5%",
+      height: 36,
+      width: "60%",
+      border: "1px solid #014949",
+      borderRadius: 16,
+      backgroundColor: "#007373",
+      alignSelf: "center",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      paddingTop: 10,
+      paddingBottom: 10,
+    },
+    text: {
+      color: "#fff",
+    },
+});
+
+export default styles;
